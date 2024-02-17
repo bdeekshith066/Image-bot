61a961d40e0f7c596442bbbe71a303978b364041
Deekshith B <130782878+bdeekshith066@users.noreply.github.com>
2024-02-17 19:13:40 +0530

    Create code.py

---
+++ code.py
@@ -0,0 +1,17 @@
+import requests
+
+# Make a POST request to the ClipDrop API endpoint
+r = requests.post('https://clipdrop-api.co/text-to-image/v1',
+  files = {
+      'prompt': (None, 'elon musk and mark zuckerberg ufc fight', 'text/plain')
+  },
+  headers = { 'x-api-key':'clipboard api key'}
+)
+
+# Check if the request was successful
+if r.ok:
+    # Save the image to a file
+    with open('output_image.jpg', 'wb') as f:
+        f.write(r.content)
+else:
+    r.raise_for_status()
